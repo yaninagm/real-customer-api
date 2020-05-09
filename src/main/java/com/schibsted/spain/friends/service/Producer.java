ec193ad95dbb2e6b59b4f54a3642c66db248915f
yamorales <yanigmorales@gmail.com>
2020-05-08 22:34:35 -0300

    Beans need contructor method, fix delete autowired

--- src/main/java/com/schibsted/spain/friends/service/Producer.java
+++ src/main/java/com/schibsted/spain/friends/service/Producer.java
@@ -1,16 +1,19 @@
-package com.schibsted.spain.friends.service.consumer;
+package com.schibsted.spain.friends.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class Producer {
     //private static final Logger logger = (Logger) LoggerFactory.getLogger(KafkaProperties.Producer.class);
     private static final String TOPIC = "users";
 
-    @Autowired
+    //@Autowired
     //private KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String message) {
         //logger.info(String.format("#### -> Producing message -> %s", message));
+        //String.format("#### -> Producing message -> %s", message);
+        System.out.println("#### -> Producing message -> %s "+ message);
         //this.kafkaTemplate.send(TOPIC, message);
     }
 }
