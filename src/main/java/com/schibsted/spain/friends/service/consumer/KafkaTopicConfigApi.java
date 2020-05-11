b42f44d53841c44939c056ed74d093cf349d54f7
yamorales <yanigmorales@gmail.com>
2020-05-11 18:19:35 -0300

    create producer

--- src/main/java/com/schibsted/spain/friends/service/consumer/KafkaTopicConfigApi.java
+++ src/main/java/com/schibsted/spain/friends/service/consumer/KafkaTopicConfigApi.java
@@ -10,14 +10,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
-public class KafkaTopicConfig {
+public class KafkaTopicConfigApi {
     //@Value(value = "${kafka.bootstrapAddress}")
-    private String bootstrapAddress;
+    private String bootstrapAddress2;
 
     @Bean
     public KafkaAdmin kafkaAdmin() {
         Map<String, Object> configs = new HashMap<>();
-        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress2);
         return new KafkaAdmin(configs);
     }
 
