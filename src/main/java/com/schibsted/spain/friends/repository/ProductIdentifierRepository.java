8f45f76adc84c5a5755669161585cd3c75cccf24
Yanina Morales <yanigmorales@gmail.com>
2020-12-09 23:54:31 +0100

    fuerzo la query

--- src/main/java/com/schibsted/spain/friends/repository/ProductIdentifierRepository.java
+++ src/main/java/com/schibsted/spain/friends/repository/ProductIdentifierRepository.java
@@ -2,11 +2,14 @@ package com.schibsted.spain.friends.repository;
 
 import com.schibsted.spain.friends.model.ProductIdentifier;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface ProductIdentifierRepository extends JpaRepository<ProductIdentifier, Long> {
+
+    @Query(value = "SELECT t FROM ProductIdentifier t where t.urlQrId = :urlQrId")
     public List<ProductIdentifier> findByUrlQrId(String urlQrId);
 }
