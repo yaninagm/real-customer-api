9f1bcc8441540acdd0d2fe8b570cc692c9bfa3b6
Yanina Morales <yanigmorales@gmail.com>
2020-12-09 23:42:41 +0100

    saco search a la base

--- src/main/java/com/schibsted/spain/friends/service/GenerateQrService.java
+++ src/main/java/com/schibsted/spain/friends/service/GenerateQrService.java
@@ -14,11 +14,11 @@ public class GenerateQrService {
 
     public String getDinamicUrl(String urlQrId) {
         Date newDate = new Date();
-        ProductIdentifier productIdentifier = productIdentifierRepository.findByUrlQrId(urlQrId).get(0);
+        //ProductIdentifier productIdentifier = productIdentifierRepository.findByUrlQrId(urlQrId).get(0);
 
 
         String qrForUse = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=ENCODEURL(https://qr-generator-redirect.herokuapp.com/qrByProduct/?id="+urlQrId+")";
-        /*
+
         ProductIdentifier productIdentifier = new ProductIdentifier(
                 1L,
                 "terreno",
@@ -26,7 +26,7 @@ public class GenerateQrService {
                 "https://www.remax.com.ar/es-ar/propiedades/cabana/venta/el-carrizal/barrio-el-coral/420971002-109?LFPNNSource=RecentlyListedOfficeProperties&cKey=420971002-109",
                 newDate,
                 "description");
-*/
+
         //https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=ENCODEURL(https://www.remax.com.ar/es-ar/propiedades/cabana/venta/el-carrizal/barrio-el-coral/420971002-109?LFPNNSource=RecentlyListedOfficeProperties&cKey=420971002-109",
         //                "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=ENCODEURL(https://www.remax.com.ar/es-ar/propiedades/cabana/venta/el-carrizal/barrio-el-coral/420971002-109?LFPNNSource=RecentlyListedOfficeProperties&cKey=420971002-109",
         return productIdentifier.getDinamicUrl();
